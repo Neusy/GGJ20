db1978a341d3d85ee9949850511ae1f293369014
Michal Cesnek <n3usyx@gmail.com>
2020-02-01 22:27:19 +0100

    Adjusted ChemeSplash

---
+++ Assets/Scripts/ChangeScheme.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScheme : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    IEnumerator Coroutine()
+    {
+      
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("MainMenu");
+    }
+    void Upload()
+    {
+
+    }
+}
