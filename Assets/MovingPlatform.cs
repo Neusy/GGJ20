e040b3d49202c974d4949ad87860221eda8e4b3e
= <b.papa1@studenti.unimol.it>
2020-02-01 02:43:58 +0100

    script piattaforme

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public float xSpeed = 0f, ySpeed = 0f, rightBoundary = 0f, leftBoundary = 0f, upperBoundary = 0f, bottomBo = 0f;
+    bool moveRight = true, moveUp = true;
+    float x = 0f, y = 0f;
+
+
+    void Start() {
+        x = transform.position.x;
+        y = transform.position.y;
+    }
+
+/*    public parameters(float _xSpeed, float _ySpeed, float _rightBorder, float _leftBorder, float _upperBorder, float _bottomBorder) {
+        xSpeed = _xSpeed;
+        ySpeed = _ySpeed;
+        rightBoundary = _rightBorder + transform.position.x;
+        leftBoundary = _leftBorder - transform.position.x;
+        upperBoundary = _upperBorder + transform.position.y;
+        bottomBo = _bottomBorder - transform.position.y;
+    }*/
+
+    void Update () {
+
+        if (transform.position.x > rightBoundary + x) 
+            moveRight = false;
+        if (transform.position.y > upperBoundary + y)
+            moveUp = false;
+        if (transform.position.x < x - rightBoundary) 
+            moveRight = true;
+        if (transform.position.y < y - upperBoundary)
+            moveUp = true;
+    
+
+        if (moveRight)
+            transform.position = new Vector2(transform.position.x + xSpeed * Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - xSpeed * Time.deltaTime, transform.position.y);
+
+        if (moveUp)
+            transform.position = new Vector2(transform.position.x, transform.position.y + ySpeed * Time.deltaTime);
+        else
+            transform.position = new Vector2(transform.position.x, transform.position.y - ySpeed * Time.deltaTime);
+        
+        
+    }
+
+}
